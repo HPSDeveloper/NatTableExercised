d6974ea192aefcfe5387d8458b8b66b71ddb0991
hs.a@talsicht.ch <hs.a@talsicht.ch>
2023-03-15 18:20:17 +0100

    Added demonstration how to insert a cell into a NatTable that has text and and icon and both trigger a different mouse click action.

--- README.md
+++ README.md
@@ -10,5 +10,14 @@ This has the advantage for me that I can explore the examples better with help o
 
  
 ## Usage 2: Adapted personal exercises:
-### Usage 2.1 software.schmid.eclipse.nattable.tryout.hierarchicalheadersforrowandcolumn.ExpandColumnAndRowHeadersMain
-Exercise: Create table that has expand collapse feature on columns and on rows.
+### Usage 2.1 ExpandColumnAndRowHeadersMain
+Location: software.schmid.eclipse.nattable.tryout.hierarchicalheadersforrowandcolumn.ExpandColumnAndRowHeadersMain
+##### Features demonstrated:
+
+* 2013.02.01: Exercise: Create table that has expand collapse feature on columns and on rows.
+* 2023.03.15: Cell with image and text both constituting hyper separate links (Clicks on text currently only functional in header columns)
+
+<br>
+The result Nat Table currently looks like this:<br>
+
+![Current result NatTable](exercises_output/ExpandColumnAndRowHeadersTable_out.png)
--- src/main/java/software/schmid/eclipse/nattable/tryout/hierarchicalheadersforrowandcolumn/ExpandColumnAndRowHeadersTable.java
+++ src/main/java/software/schmid/eclipse/nattable/tryout/hierarchicalheadersforrowandcolumn/ExpandColumnAndRowHeadersTable.java
@@ -2,13 +2,16 @@ package software.schmid.eclipse.nattable.tryout.hierarchicalheadersforrowandcolu
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.nebula.widgets.nattable.NatTable;
+import org.eclipse.nebula.widgets.nattable.config.AbstractRegistryConfiguration;
+import org.eclipse.nebula.widgets.nattable.config.CellConfigAttributes;
+import org.eclipse.nebula.widgets.nattable.config.DefaultNatTableStyleConfiguration;
+import org.eclipse.nebula.widgets.nattable.config.IConfigRegistry;
 import org.eclipse.nebula.widgets.nattable.data.ExtendedReflectiveColumnPropertyAccessor;
 import org.eclipse.nebula.widgets.nattable.data.IColumnPropertyAccessor;
 import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
@@ -25,15 +28,28 @@ import org.eclipse.nebula.widgets.nattable.hideshow.ColumnHideShowLayer;
 import org.eclipse.nebula.widgets.nattable.layer.CompositeLayer;
 import org.eclipse.nebula.widgets.nattable.layer.DataLayer;
 import org.eclipse.nebula.widgets.nattable.layer.ILayer;
+import org.eclipse.nebula.widgets.nattable.layer.cell.ColumnLabelAccumulator;
+import org.eclipse.nebula.widgets.nattable.painter.cell.ImagePainter;
+import org.eclipse.nebula.widgets.nattable.painter.cell.TextPainter;
+import org.eclipse.nebula.widgets.nattable.painter.cell.decorator.CellPainterDecorator;
 import org.eclipse.nebula.widgets.nattable.reorder.ColumnReorderLayer;
 import org.eclipse.nebula.widgets.nattable.selection.SelectionLayer;
 import org.eclipse.nebula.widgets.nattable.selection.config.DefaultRowSelectionLayerConfiguration;
+import org.eclipse.nebula.widgets.nattable.style.DisplayMode;
 import org.eclipse.nebula.widgets.nattable.tree.TreeLayer;
+import org.eclipse.nebula.widgets.nattable.ui.action.IMouseAction;
+import org.eclipse.nebula.widgets.nattable.ui.binding.UiBindingRegistry;
+import org.eclipse.nebula.widgets.nattable.ui.matcher.CellPainterMouseEventMatcher;
+import org.eclipse.nebula.widgets.nattable.ui.matcher.MouseEventMatcher;
+import org.eclipse.nebula.widgets.nattable.ui.util.CellEdgeEnum;
 import org.eclipse.nebula.widgets.nattable.viewport.ViewportLayer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.MessageBox;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -42,6 +58,11 @@ import ca.odell.glazedlists.EventList;
 import ca.odell.glazedlists.GlazedLists;
 import ca.odell.glazedlists.TreeList;
 
+/**
+ * This class exercises the creation of a Eclipse Nebula NatTable that has grouped columns and rows which may be collapsed and expanded.
+ * Additional features demonstrated are:
+ * 2023.03.15: Cell with image and text both constituting hyper separate links (Clicks on text currently only functional in header columns)
+ */
 public class ExpandColumnAndRowHeadersTable {
 	Map<String,String> propertyToLabelMap = ImmutableMap.of("name", "Name", "population", "Population");
 	String[] propertyNames = {"content.name", "content.population"};
@@ -58,6 +79,8 @@ public class ExpandColumnAndRowHeadersTable {
 		
 		DataLayer bodyDataLayer = new DataLayer(dataProvider);
 		setColumnWidth(bodyDataLayer, withScrollbars);
+		// Provides that all body cells are attached a label ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + <column_index>:
+        bodyDataLayer.setConfigLabelAccumulator(new ColumnLabelAccumulator() );
 		
 		final ColumnReorderLayer columnReorderLayer = new ColumnReorderLayer(bodyDataLayer, true);
 		final ColumnHideShowLayer columnHideShowLayer = new ColumnHideShowLayer(columnReorderLayer);
@@ -82,6 +105,8 @@ public class ExpandColumnAndRowHeadersTable {
 				propertyToLabelMap);
 		DataLayer headerDataLayer  = new DataLayer(headerDataProvider);
 		ILayer columnHeaderLayer = new ColumnHeaderLayer(headerDataLayer, viewPortLayerBody, selectionLayer);
+		// Provides that all header cells are attached a label ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + <column_index>:
+		headerDataLayer.setConfigLabelAccumulator(new ColumnLabelAccumulator());
 		
 		ColumnGroupHeaderLayer columnGroupHeaderLayer = new ColumnGroupHeaderLayer(columnHeaderLayer, selectionLayer, columnGroupModel);
 		int[] columnIndexes = {0,1};
@@ -92,12 +117,82 @@ public class ExpandColumnAndRowHeadersTable {
 		compositeLayer.setChildLayer(GridRegion.COLUMN_HEADER, columnGroupHeaderLayer, 0,  0);
 		compositeLayer.setChildLayer(GridRegion.BODY,  viewPortLayerBody, 0,  1);
 		
-		NatTable natTable = new NatTable(parent, SWT.V_SCROLL | SWT.H_SCROLL, compositeLayer);
+		
+		NatTable natTable = new NatTable(parent, SWT.V_SCROLL | SWT.H_SCROLL, compositeLayer, false);
+		natTable.addConfiguration(new DefaultNatTableStyleConfiguration());
 		natTable.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
+		
+		
+		// Prepare a text link and a icon link in one cell:
+        final CellPainterDecorator textAndIconLinkDecorator = getPainterCombiningTextLinkAndIconLinkInOneCell();  
+        
+        // Register the cell with its text and icon as separate links within the NatTable:
+        natTable.addConfiguration(createConfigForLinkedIconAndLinkedText(textAndIconLinkDecorator));
+		
 		natTable.configure();
 		
 		GridDataFactory.fillDefaults().grab(true,  true).applyTo(natTable);
 		
+	}
+
+
+	/**
+	 * Get a CellPainterDecorator that inserts a text and an icon into the cell
+	 */
+	private CellPainterDecorator getPainterCombiningTextLinkAndIconLinkInOneCell() {
+		TextPainter textLink = new TextPainter();
+        ImagePainter imageLink = new ImagePainter(new Image(Display.getDefault(),
+        		ExpandColumnAndRowHeadersTable.class.getResourceAsStream("baseline_subway_black_48dp.png")));
+        final CellPainterDecorator textAndIconLinkDecorator = 
+                new CellPainterDecorator(textLink, CellEdgeEnum.RIGHT, imageLink);
+		return textAndIconLinkDecorator;
+	}
+
+
+	/**
+	 * Register the cell that contains a text and an icon painter with the NatTable. 
+	 * The text and the icon shall trigger different link action
+	 */
+	private AbstractRegistryConfiguration createConfigForLinkedIconAndLinkedText(final CellPainterDecorator textWithIconSeparateLinksDecorator) {
+		return new AbstractRegistryConfiguration() {
+            @Override
+            public void configureRegistry(IConfigRegistry configRegistry) {
+                configRegistry.registerConfigAttribute(
+                        CellConfigAttributes.CELL_PAINTER,
+                        textWithIconSeparateLinksDecorator, DisplayMode.NORMAL,
+                        ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + 1);
+            }
+
+            @Override
+            public void configureUiBindings(UiBindingRegistry uiBindingRegistry) {
+                uiBindingRegistry.registerFirstSingleClickBinding(
+                        new CellPainterMouseEventMatcher(
+                                GridRegion.COLUMN_HEADER,
+                                MouseEventMatcher.LEFT_BUTTON,
+                                textWithIconSeparateLinksDecorator.getBaseCellPainter()),
+                        new IMouseAction() {
+
+							@Override
+							public void run(NatTable arg0, MouseEvent arg1) {
+								openMessageDialogue("Textfield clicked!!!");
+							}
+                        	
+                        });
+
+                uiBindingRegistry.registerFirstSingleClickBinding(
+                        new CellPainterMouseEventMatcher(
+                                GridRegion.COLUMN_HEADER,
+                                MouseEventMatcher.LEFT_BUTTON,
+                                textWithIconSeparateLinksDecorator.getDecoratorCellPainter()),
+                        new IMouseAction() {
+
+							@Override
+							public void run(NatTable arg0, MouseEvent arg1) {
+								openMessageDialogue("Icon clicked!!!");
+							}
+                        });
+            }
+        };
 	}
 	
 
@@ -131,5 +226,15 @@ public class ExpandColumnAndRowHeadersTable {
 		
 		return ImmutableList.of(globe, europe, switzerland, germany, russia, asia, japan, china, india);
 	}
+	
+    private void openMessageDialogue(String text) {
+    	MessageBox dialog =
+    		    new MessageBox(Display.getDefault().getActiveShell(), SWT.ICON_QUESTION | SWT.OK| SWT.CANCEL);
+    		dialog.setText("Link was clicked");
+    		dialog.setMessage(text);
+
+    		// open dialog and await user selection
+    		dialog.open();
+    }
 
 }
